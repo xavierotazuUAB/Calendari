5b3049ee3207aaeb938da7b2e3fb3519fced9b8f
xavierotazuUAB <xavier.otazu@uab.cat>
2025-09-25 17:31:16 +0200

    Primera versió del mètode DiesDelMes()
    Hem definit un conjunt de casos de prova pel cas més fàcil: un any que NO és de traspàs. Hem posat tots els 12 mesos ja que són pocs casos.
    Hem definit un mètode public per tal de poder fer testing d'un mètode provat.
    Hem implementat el mètode setUp().

--- src/calendari/SecondsCalculator.java
+++ src/calendari/SecondsCalculator.java
@@ -5,5 +5,16 @@ public class SecondsCalculator
 	public long SegonsFins(int dia, int mes, int any, int hora, int minut, int segon);
 	
 	private boolean IsAnyTraspas(int any);
-	private int DiesDelMes(int mes, int any);
+	
+	private int DiesDelMes(int mes, int any)
+	{
+		int dies[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		return dies[mes-1];
+	}
+
+	
+	// Mètodes publics per poder fer testing
+	
+	public int getDiesDelMes(int mes, int any) {return DiesDelMes(mes,any);}
 }
--- src/calendari/SecondsCalculatorTest.java
+++ src/calendari/SecondsCalculatorTest.java
@@ -7,16 +7,31 @@ import org.junit.jupiter.api.Test;
 
 class SecondsCalculatorTest
 {
-
+	SecondsCalculator calc;
+	
 	@BeforeEach
 	void setUp() throws Exception
 	{
+		// Creació de l'objecte calc per poder cridar-lo des dels mètodes de prova
+		calc = new SecondsCalculator();
 	}
 
 	@Test
-	void test()
+	void testDiesDelMes()
 	{
-		fail("Not yet implemented");
+		// Cas més fàcil: anys no multiples de 4. 
+		assertEquals(calc.getDiesDelMes(1,2001), 31);
+		assertEquals(calc.getDiesDelMes(2,2001), 28);
+		assertEquals(calc.getDiesDelMes(3,2001), 31);
+		assertEquals(calc.getDiesDelMes(4,2001), 30);
+		assertEquals(calc.getDiesDelMes(5,2001), 31);
+		assertEquals(calc.getDiesDelMes(6,2001), 30);
+		assertEquals(calc.getDiesDelMes(7,2001), 31);
+		assertEquals(calc.getDiesDelMes(8,2001), 31);
+		assertEquals(calc.getDiesDelMes(9,2001), 30);
+		assertEquals(calc.getDiesDelMes(10,2001), 31);
+		assertEquals(calc.getDiesDelMes(11,2001), 30);
+		assertEquals(calc.getDiesDelMes(12,2001), 31);
 	}
 
 }
