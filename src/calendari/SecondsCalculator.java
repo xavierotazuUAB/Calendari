1e13fcbec96a98bbd656b79480b51e35184cd7eb
xavierotazuUAB <xavier.otazu@uab.cat>
2025-09-25 16:46:51 +0200

    Versió inicial
    Definició inicial de la classe SecondsCalculator i la seva classe de test associada.

---
+++ src/calendari/SecondsCalculator.java
@@ -0,0 +1,9 @@
+package calendari;
+
+public class SecondsCalculator
+{
+	public long SegonsFins(int dia, int mes, int any, int hora, int minut, int segon);
+	
+	private boolean IsAnyTraspas(int any);
+	private int DiesDelMes(int mes, int any);
+}
---
+++ src/calendari/SecondsCalculatorTest.java
@@ -0,0 +1,22 @@
+package calendari;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SecondsCalculatorTest
+{
+
+	@BeforeEach
+	void setUp() throws Exception
+	{
+	}
+
+	@Test
+	void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
